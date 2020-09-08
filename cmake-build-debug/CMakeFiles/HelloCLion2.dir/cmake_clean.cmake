41aa5be1a11811806c7a8cf6788dcbb13fef6d1a
Max <Maxumane@gmail.com>
2020-09-08 12:17:31 -0700

    This is my first commit in my HelloClion2 file.

---
+++ cmake-build-debug/CMakeFiles/HelloCLion2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloCLion2.dir/main.cpp.o"
+  "HelloCLion2.exe"
+  "HelloCLion2.pdb"
+  "libHelloCLion2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloCLion2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
